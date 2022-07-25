3694c473da458c738aeccb2b60f7ecacf9a234bc
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-07-25 20:33:46 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import './App.module.css';
 import React from 'react';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 import { Container } from './index';
 
 import preloader from '../images/img/loading.gif';
@@ -10,19 +10,9 @@ const Landing = lazy(() => import('../components/Landing'));
 function App() {
   return (
     <>
-      <Suspense
-        fallback={
-          <img
-            src={preloader}
-            alt="Preloader"
-            style={{ position: 'absolute', top: '10%', left: '10%' }}
-          />
-        }
-      >
-        <Container>
-          <Landing />
-        </Container>
-      </Suspense>
+      <Container>
+        <Landing />
+      </Container>
     </>
   );
 }
